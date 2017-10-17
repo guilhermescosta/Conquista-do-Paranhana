c34164488527530cdf6ace6f2cea700ea0d5bd9f
TellesBruno <32803605+TellesBruno@users.noreply.github.com>
2017-10-17 19:33:32 -0200

    Add files via upload

---
+++ Assets/Scripts/InteracaoGold.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteracaoGold : MonoBehaviour {
+	public bool isTake;
+	public SpriteRenderer sr2;
+	void Start () {
+		sr2 = GetComponent<SpriteRenderer> ();
+		sr2.color = new Color (1, 1, 1, 1);
+	}
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			isTake = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			sr2.color = new Color (1, 1, 1, 1);
+			isTake = false;
+		}
+	}
+
+	void FixedUpdate(){
+		if (isTake && Input.GetKeyDown (KeyCode.E)) {
+			sr2.color = new Color (0, 0, 0, 0);
+			Debug.Log ("50 de Gold adicionado");
+			gameObject.SetActive (false);
+			LevelManager.levelManager.SetCoin (50);
+		}
+	}
+}
