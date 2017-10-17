98295fd5f898b1f01821775694304e3db53da004
TellesBruno <32803605+TellesBruno@users.noreply.github.com>
2017-10-17 19:43:39 -0200

    Add files via upload

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public static LevelManager levelManager; 
+
+	private int Coin = 0;
+	private bool gameOver = false;
+	public Text CoinText;
+
+
+	// Use this for initialization
+	void Awake () {
+
+		if (levelManager == null) {
+			levelManager = this;
+		} else if (levelManager != this) {
+			Destroy (gameObject);
+		}
+	}
+	public void SetCoin(int x)
+	{
+		Coin = Coin + x;
+		CoinText.text = Coin.ToString ();
+		Debug.Log (Coin);
+	
+	}
+
+	public int GetCoin()
+	{
+		return Coin;
+    }
+}
+
