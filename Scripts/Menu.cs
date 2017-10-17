41c16a2f149984963b77c948f9e9dcac4eb2c33a
guilhermescosta <guilhermescosta@gmail.com>
2017-10-17 17:17:25 -0200

    teste

---
+++ Scripts/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour {
+
+	public string newGame;
+	public string loadGame;
+	public string options;
+	public string exitGame;
+
+	public void NewGame(){
+	
+		//SceneManager.LoadScene (newGame);
+		Debug.Log("NewGamebtn");
+	}
+
+	public void LoadGame(){
+
+		//SceneManager.LoadScene (loadGame);
+		Debug.Log("LoadGamebtn");
+
+	}
+
+	public void Options(){
+
+		//SceneManager.LoadScene (options);
+		Debug.Log("Optionbtn");
+	}
+
+	public void ExitGame(){
+
+		//SceneManager.LoadScene (exitGame);
+		Debug.Log("ExitGamebtn");
+	}
+
+
+}
---
+++ Scripts/PassaDeFase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassaDeFase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log (other.name);
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log (other.name);
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
