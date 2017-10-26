f6cac7823b999b2ad8d323bd71c7f47b95641449
guilhermescosta <guilhermescosta@gmail.com>
2017-10-26 09:53:55 -0200

    Add files via upload

--- Assets/Scripts/InteracaoNPC.cs
+++ Assets/Scripts/InteracaoNPC.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteracaoNPC : MonoBehaviour {
 	private bool isTalk;
+	public GameObject talkBalloon;
+	public string[] phrases;
+	public Text phrasesText;
+	public int indexPhrase;
+
+	/*void Start() {
+		indexPhrase = 0;
+	}*/
+
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			isTalk = true;
+			indexPhrase = 0;
 		}
 	}
 	void OnTriggerExit2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			isTalk = false;
+			indexPhrase = 0;
 		}
 	}
 
 	void FixedUpdate(){
-		if (isTalk && Input.GetKeyDown (KeyCode.E))
-			Debug.Log ("Oi eu sou o Goku");
+		if (isTalk && Input.GetKeyDown (KeyCode.E) && indexPhrase <= phrases.Length - 1) {
+			phrasesText.text = phrases [indexPhrase];
+			talkBalloon.SetActive (true);
+			indexPhrase++;
+		} else if (isTalk  && Input.GetKeyDown (KeyCode.E) && indexPhrase >= phrases.Length - 1){
+			talkBalloon.SetActive (false);
+		}
 	}
 }
+
--- Assets/Scripts/movimento.cs
+++ Assets/Scripts/movimento.cs
@@ -5,23 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class movimento : MonoBehaviour {
 
+	public int exit;
+	public GameObject talkBalloon;
+
+	void Start(){
+		DontDestroyOnLoad (this);
+		Debug.Log (exit);
+	}
+
 	void FixedUpdate () {
 
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Translate (0, -.1f, 0);
-		}
+		if (talkBalloon.activeSelf == false) {
 
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.Translate (0, .1f, 0);
-		}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				transform.Translate (0, -.1f, 0);
+			}
 
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Translate (-.1f, 0, 0);
-		}
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				transform.Translate (0, .1f, 0);
+			}
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Translate (.1f, 0, 0);
-			//SceneManager.LoadScene ();
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Translate (-.1f, 0, 0);
+			}
+
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.Translate (.1f, 0, 0);
+				//SceneManager.LoadScene ();
+			}
 		}
 	}
 }
