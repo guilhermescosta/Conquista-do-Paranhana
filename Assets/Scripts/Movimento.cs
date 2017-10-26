c35b9cf6c683a2444dc879fd8e4d0a6ea05609f3
guilhermescosta <guilhermescosta@gmail.com>
2017-10-26 18:14:28 -0200

    Merge + Commit 26/10
    - interacao com NPCs
    - Transicao de telas

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movimento : MonoBehaviour {
+
+	public int exit;
+	public GameObject talkBalloon;
+
+	void Start(){
+		DontDestroyOnLoad (this);
+		Debug.Log (exit);
+		talkBalloon = GameObject.Find ("Balao");
+	}
+
+	void FixedUpdate () {
+
+		if (talkBalloon.GetComponent<Image>().enabled==false) {
+
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				transform.Translate (0, -.1f, 0);
+			}
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				transform.Translate (0, .1f, 0);
+			}
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Translate (-.1f, 0, 0);
+			}
+
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				transform.Translate (.1f, 0, 0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PassaDeFase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassaDeFase : MonoBehaviour {
+
+	public Movimento _mov;
+	public GameObject _player;
+	public GameObject playerStart;   // posicao de saida
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.Find ("Player");
+		_mov= _player.GetComponent<Movimento> ();
+
+		if (_mov.exit==1){
+			_player.transform.position = new Vector3 (-4, 0, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log (other.name);
+		if (other.gameObject.CompareTag("Player"))
+		{
+			_mov.exit = 1;
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
+		}
+	}
+}
