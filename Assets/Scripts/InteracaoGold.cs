936ce9acbc02811e4666770fb85d845ac6dbe12c
TellesBruno <32803605+TellesBruno@users.noreply.github.com>
2017-10-29 16:19:57 -0200

    Add files via upload

--- Assets/Scripts/InteracaoGold.cs
+++ Assets/Scripts/InteracaoGold.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class InteracaoGold : MonoBehaviour {
 	public bool isTake;
+	public bool vAction;
 	public SpriteRenderer sr2;
 	public Sprite sTronco;
 	void Start () {
 		sr2 = GetComponent<SpriteRenderer> ();
-
+		vAction = true;
 	}
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
@@ -22,12 +23,13 @@ public class InteracaoGold : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		if (isTake && Input.GetKeyDown (KeyCode.E)) {
+		if (isTake && Input.GetKeyDown (KeyCode.E)&& vAction) {
 			sr2.sprite = sTronco;
 			Debug.Log ("50 de Gold adicionado");
 			LevelManager.levelManager.SetCoin (50);
 			LevelManager.levelManager.SetSede (5);
 			LevelManager.levelManager.SetFome (10);
+			vAction = false;
 		}
 	}
 }
