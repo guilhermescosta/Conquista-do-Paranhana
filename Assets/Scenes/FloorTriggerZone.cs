41845a5c654d116dd29b4612fe9f554151b8228a
Victor B. Dornelles <32820715+Victor-BwD@users.noreply.github.com>
2017-11-05 16:05:31 -0200

    Add files via upload

---
+++ Assets/Scenes/FloorTriggerZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTriggerZone : MonoBehaviour {
+
+	public bool isDamaging;
+	public float damage = 10;
+
+	private void OnTriggerStay2D(Collider2D col)
+	{
+		if(col.tag == "Player")
+			col.SendMessage((isDamaging)?"TakeDamage":"HealDamage",Time.deltaTime * damage);
+	}
+}
