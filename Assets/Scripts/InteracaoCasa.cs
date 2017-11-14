7e41530abec3e42772e2023aebb2bc5823e22233
guilhermescosta <guilhermescosta@gmail.com>
2017-11-14 10:47:13 -0200

    commit 14/11
    Inventario + datas

---
+++ Assets/Scripts/InteracaoCasa.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteracaoCasa : MonoBehaviour {
+
+	private bool isHome;
+	public SpriteRenderer _home;
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.gameObject.tag == "Player" && isHome == false) {
+		
+			isHome = true;
+		
+		} else { 
+			isHome = false;
+		}
+	
+	}
+
+
+	void FixedUpdate (){
+	
+		if (isHome) {
+
+			_home.color = Color.black;
+
+		} else {
+		
+			_home.color = Color.white ;
+		}
+	
+	
+	}
+
+
+}
